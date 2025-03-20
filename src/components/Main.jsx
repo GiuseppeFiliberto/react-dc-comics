f769bbf98e87ff483bffbb352a4353cdebbd145f
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-03-20 16:19:53 +0100

    add Main component, update App to include it, and define CSS variables

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+export default function Main() {
+
+    return (
+        <>
+            <div className="bg-dark">
+                <div className="container text-white py-5">
+                    <h4 className="fw-bolder">--&gt; Content goes here &lt;--</h4>
+                </div>
+            </div>
+
+        </>
+    )
+}
