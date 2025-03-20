abd4896d94b08a5bcf485d7c9473de5a37c6e5bf
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-03-20 16:31:46 +0100

    add blue banner section to Main component and update CSS for styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,6 +8,17 @@ export default function Main() {
                 </div>
             </div>
 
+            {/* // Blue Section */}
+
+            <div className="blue-banner-info text-white py-5">
+                <div className="container d-flex justify-content-around">
+                    <div className="d-flex align-items-center gap-3">
+                        <img src="../img/buy-comics-digital-comics.png" alt="Digital Comics" width="50" />
+                        <span>DIGITAL COMICS</span>
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
