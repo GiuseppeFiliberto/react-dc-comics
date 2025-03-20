96f09b5e6de819e8283e9fac5ab30b2a5d4ddb02
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-03-20 18:51:00 +0100

    refactor Header component layout; enhance navigation styles and add hover effects

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,19 @@ export default function Header() {
                         <a className="logo p-2" href="#">
                             <img src="../img/dc-logo.png" alt="DC Logo" width="80" />
                         </a>
-                        <ul className="navbar-nav ms-auto nav-ul">
-                            <li className="nav-item"><a className="nav-link" href="#">CHARACTERS</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">COMICS</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">MOVIES</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">TV</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">GAMES</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">COLLECTIBLES</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">VIDEOS</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">FANS</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">NEWS</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">SHOP</a></li>
+                        <ul className="navbar-nav ms-auto nav-ul p-3">
+                            <li><a href="#">CHARACTERS</a></li>
+                            <li><a href="#">COMICS</a></li>
+                            <li><a href="#">MOVIES</a></li>
+                            <li><a href="#">CHARACTERS</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">GAMES</a></li>
+                            <li><a href="#">COLLECTIBLES</a></li>
+                            <li><a href="#">VIDEOS</a></li>
+                            <li><a href="#">FANS</a></li>
+                            <li><a href="#">NEWS</a></li>
+                            <li><a href="#">SHOP</a></li>
+
                         </ul>
                     </div>
                 </nav>
